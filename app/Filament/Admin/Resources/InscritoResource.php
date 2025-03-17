9af65e62405c2ce9f0b32807ba7fd392aa36f640
Hélio <heliocg@zoho.com>
2025-03-17 14:28:29 -0300

    Ajustes

--- app/Filament/Admin/Resources/InscritoResource.php
+++ app/Filament/Admin/Resources/InscritoResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Admin\Resources;
 
+use App\Enums\InvoiceStatus;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Tables;
 use App\Models\Inscrito;
@@ -9,12 +11,15 @@ use Filament\Forms\Form;
 use App\Models\Inscricao;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Admin\Resources\InscritoResource\Pages;
 use App\Filament\Admin\Resources\InscritoResource\RelationManagers;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 
 class InscritoResource extends Resource
 {
@@ -28,7 +33,12 @@ class InscritoResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('nome_usual_ele'),
+                TextInput::make('nome_usual_ela'),
+                Select::make('status_pagamento')
+                    ->label('Status de pagamento')
+                    ->options(InvoiceStatus::class)
+                    ->searchable()
             ]);
     }
 
@@ -46,7 +56,14 @@ class InscritoResource extends Resource
                 TextColumn::make('telefone')
                     ->formatStateUsing(fn ($state) => preg_replace('/(\d{2})(\d{5})(\d{4})/', '($1) $2-$3', $state)),
                 TextColumn::make('status_pagamento')
-                    ->badge(),
+                    ->label('Status do Pagamento')
+                    ->badge()
+                    ->color(fn ($record) => match ($record->status_pagamento) {
+                        'Pago' => 'success',  // Verde
+                        'Pendente' => 'warning', // Amarelo
+                        'Cancelado' => 'danger', // Vermelho
+                        default => 'secondary', // Cinza para outros casos
+                    }),
                 TextColumn::make('paymentDate')
                     ->label('Pago em'),
                 TextColumn::make('created_at')
@@ -56,7 +73,23 @@ class InscritoResource extends Resource
                 //
             ])
             ->actions([
-                //Tables\Actions\EditAction::make(),
+
+                Action::make('marcarPago')
+                    ->label('Marcar Pago')
+                    ->icon('heroicon-o-check-circle')
+                    ->button()
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->modalHeading(fn ($record) => "Confirmar Pagamento de {$record->nome_ele} & {$record->nome_ela}?")
+                    ->action(fn ($record) => $record->update([
+                        'paymentDate' => Carbon::now(),
+                        'status_pagamento' => 'Pago',
+                    ]))
+                    ->successNotificationTitle('Pagamento confirmado com sucesso!')
+                    ->hidden(fn ($record) => $record->status_pagamento === 'Pago'),
+                Tables\Actions\EditAction::make()
+                    ->label('Editar')
+                    ->button(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
