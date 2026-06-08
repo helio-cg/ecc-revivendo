27e55643cadf07335b2760ee768a7d86da01ab8b
Hélio <helio@elicast.app>
2026-06-08 09:51:07 -0300

    Ajustes

--- app/Http/Controllers/ConsultarInscricao.php
+++ app/Http/Controllers/ConsultarInscricao.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inscricao;
+use App\Models\Paroquia;
+use OpenPix\PhpSdk\Client;
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
 
 class ConsultarInscricao extends Controller
 {
@@ -33,27 +36,69 @@ class ConsultarInscricao extends Controller
 
 
     public function mostrar($telefone)
-{
-    $telefone = preg_replace('/\D/', '', $telefone);
+    {
+        $telefone = preg_replace('/\D/', '', $telefone);
 
-    $inscricao = \App\Models\Inscricao::with(['invoice','paroquia'])
-        ->where('telefone', $telefone)
-        ->first();
-
-    if (!$inscricao) {
-        $inscricao = \App\Models\InscricaoIndividual::with(['invoice','paroquia'])
+        $inscricao = \App\Models\Inscricao::with(['invoice','paroquia'])
             ->where('telefone', $telefone)
             ->first();
+        
+        if(is_null($inscricao)) {
+            return redirect()->back()->with('error', 'Inscrição não encontrada.');
+        }
+
+        if (!$inscricao) {
+            $inscricao = \App\Models\InscricaoIndividual::with(['invoice','paroquia'])
+                ->where('telefone', $telefone)
+                ->first();
+        }
+        //dd($inscricao);
+        $openPix = app(Client::class);
+        $charge = $openPix->charges()->getOne($inscricao->invoice->transactionID);
+        if (!$charge) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nenhuma cobrança encontrada para esta inscrição'
+            ]);
+        }
+
+        if ($charge['charge']['status'] === 'EXPIRED') {
+            $paroquia = Paroquia::where('id',$inscricao['paroquia_id'])->first();
+            $nome = $inscricao['nome_usual_ele'] . ' e ' . $inscricao['nome_usual_ela'];
+            $paroquias = $paroquia->name . ' de ' . $paroquia->city;
+            
+                $dadosCobrancaCliente = [
+                    'correlationID' => Uuid::uuid7()->toString(),
+                    'value' => $inscricao->invoice->valor * 100,
+                    'additionalInfo' => [
+                        [
+                            'key' => 'Nome',
+                            'value' => $nome
+                        ],
+                        [
+                            'key' => 'Paróquia',
+                            'value' =>  $paroquias
+                        ]
+                    ],
+                ];
+            
+
+            $openPix = app(Client::class);
+            $resposta = $openPix->charges()->create($dadosCobrancaCliente);
+
+            $inscricao->invoice->update([
+                'transactionID' => $resposta['charge']['transactionID'],
+                'invoiceUrl' => $resposta['charge']['paymentLinkUrl']
+            ]);
+        }  
+
+        if (!$inscricao) {
+            return redirect()->back()->with('error', 'Inscrição não encontrada.');
+        }
+        
+        $tipo = $inscricao instanceof Inscricao ? 'casal' : 'individual';
+
+        return view('resultado-inscricao', compact('inscricao', 'tipo'));
     }
-
-    if (!$inscricao) {
-        return redirect()->back()->with('error', 'Inscrição não encontrada.');
-    }
-     $tipo = $inscricao instanceof Inscricao ? 'casal' : 'individual';
-
-    return view('resultado-inscricao', compact('inscricao', 'tipo'));
-}
-
-
 
 }
